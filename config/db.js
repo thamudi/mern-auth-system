b62c1a8cf9a6ca0ffacd12043295554309d3f79f
thamudi <tamim.hamoudi@gmail.com>
2021-04-30 12:20:46 +0300

    Backend - db connection file

---
+++ config/db.js
@@ -0,0 +1,16 @@
+'use strict';
+const mongoose = require('mongoose');
+const connectDb = async () => {
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+        //TODO: check these
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+    console.log('======================================');
+    console.info(`Mongo DB is connected to: ${connection.connection.host}`);
+    console.log('======================================');
+}
+
+module.exports = connectDb;
